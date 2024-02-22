5179abd64ffd2bfd40cea36a98081d465d010f4f
rong5525 <rong5525@gmail.com>
2024-02-22 18:10:28 -0500

    update

--- o.py
+++ o.py
@@ -1,12 +1,12 @@
 import os
 
 def generate_pdf_list_html(subfolder_path):
-    # 修改为使用 h3 标签
+    # 修改为使用 h2 标签
     html_content = '<ul style="list-style-type: none; margin: 0; padding: 0;">\n'
     for item in sorted(os.listdir(subfolder_path)):
         if item.lower().endswith('.pdf'):
             file_name_without_extension = item.rsplit('.', 1)[0]
-            html_content += f'<li style="line-height: 1;"><h3><a href="{item}">{file_name_without_extension}</a></h3></li>\n'
+            html_content += f'<li style="line-height: 1;"><h2><a href="{item}">{file_name_without_extension}</a></h2></li>\n'
     html_content += '</ul>\n'
     return html_content
 
@@ -26,13 +26,13 @@ def generate_subfolder_index_html(subfolder_path, root_folder):
         .absoluteCenter {{ width: 900px; height: 600px; position: absolute; left: 50%; top: 10%; margin-left: -450px; }}
         a {{ text-decoration: none; color: black; }}
         a:hover {{ text-decoration: underline; }}
-        h3 {{ font-size: 20px; }} /* 确保 h3 标签的字体大小适用 */
+        
     </style>
 </head>
 <body>
     <div class="absoluteCenter">
     
-        <h2><a href="../../index.html" style="text-decoration: none; color: black;">Ref</a> - {folder_name}</h2>
+        <h1><a href="../../index.html" style="text-decoration: none; color: black;">Ref</a> - {folder_name}</h1>
         {pdf_list_html}    
 
     </div>
@@ -54,13 +54,13 @@ def generate_root_index_html(root_folder):
         .absoluteCenter {{ width: 900px; height: 600px; position: absolute; left: 50%; top: 10%; margin-left: -450px; }}
         a {{ text-decoration: none; color: black; }}
         a:hover {{ text-decoration: underline; }}
-        h3 {{ font-size: 20px; }} /* 确保 h3 标签的字体大小适用 */
+       
     </style>
 </head>
 <body>
     <div class="absoluteCenter">
         
-        <h2>Ref</h2>
+        <h1>Ref</h1>
         """
 
     for first_level_folder in sorted(os.listdir(root_folder)):
@@ -72,7 +72,7 @@ def generate_root_index_html(root_folder):
                 
             subfolder_links = [f'<a href="{os.path.join(first_level_folder, f, "index.html")}">{f}</a>' for f in subfolders]
             subfolder_str = " - ".join(subfolder_links)
-            html_str += f'<h3>{first_level_folder}: {subfolder_str}</h3>\n'
+            html_str += f'<h2>{first_level_folder}: {subfolder_str}</h2>\n'
     
     html_str += "</div>\n</body>\n</html>"
 
